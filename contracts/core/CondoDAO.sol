a45216bbb24b2142fef50fe1d33066e4b82130dd
yellowth <53272104+yellowth@users.noreply.github.com>
2024-11-03 18:32:50 +0800

    (wip) feat: facility manager

--- contracts/core/CondoDAO.sol
+++ contracts/core/CondoDAO.sol
@@ -4,34 +4,40 @@ pragma solidity >=0.5.0 <0.9.0;
 import "../core/UnitManager.sol";
 import "../core/TreasuryManager.sol";
 import "../core/VotingSystem.sol";
-
+import "../core/FacilityManager.sol";
 
 import "../storage/UnitStorage.sol";
 import "../storage/TreasuryStorage.sol";
+import "../storage/FacilityStorage.sol";
 
 contract CondoDAO {
     // interfaces
     UnitManager public unitManager;
     TreasuryManager public treasuryManager;
     VotingSystem public votingSystem;
+    FacilityManager public facilityManager;
 
     // data
     UnitStorage private unitStorage;
     TreasuryStorage private treasuryStorage;
+    FacilityStorage private facilityStorage;
 
     constructor() public {
         // initialize data storage
         unitStorage = new UnitStorage();
         treasuryStorage = new TreasuryStorage();
+        facilityStorage = new FacilityStorage();
 
         // initialize interface contracts
         unitManager = new UnitManager(address(unitStorage));
         treasuryManager = new TreasuryManager(address(treasuryStorage), address(unitManager));
         votingSystem = new VotingSystem(address(unitManager));
+        facilityManager = new FacilityManager(address(facilityStorage), address(unitManager));
 
         // Set authorization for storage contracts
         unitStorage.addAuthorizedContract(address(unitManager));
         treasuryStorage.addAuthorizedContract(address(treasuryManager));
+        facilityStorage.addAuthorizedContract(address(facilityManager));
 
         // Initialize minimum reserve after authorization is set
         treasuryManager.updateMinimumReserve(10 ether);
--- contracts/interfaces/IFacilityManager.sol
+++ contracts/interfaces/IFacilityManager.sol
@@ -1,3 +1,31 @@
-// defines the contract interface
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
+
+interface IFacilityManager {
+    // Events
+    event FacilityAdded(uint256 indexed facilityId, string name);
+    event BookingMade(uint256 indexed facilityId, address indexed user, uint256 date, uint256 startHour, uint256 duration);
+    event BookingCancelled(uint256 indexed facilityId, address indexed user, uint256 date, uint256 startHour);
+
+    // View Functions
+    function getFacility(uint256 _facilityId) external view returns (
+        string memory name,
+        uint256 openTime,
+        uint256 closeTime,
+        bool active
+    );
+    
+    function getBookingDetails(uint256 _facilityId, uint256 _date, uint256 _hour) external view returns (
+        address user,
+        uint256 date,
+        uint256 startHour,
+        uint256 duration
+    );
+    
+    function checkAvailability(uint256 _facilityId, uint256 _date, uint256 _startHour, uint256 _duration) external view returns (bool);
+
+    // State-Changing Functions
+    function addFacility(string memory _name, uint256 _openTime, uint256 _closeTime) external;
+    function bookFacility(uint256 _facilityId, uint256 _date, uint256 _startHour, uint256 _duration) external;
+    function cancelBooking(uint256 _facilityId, uint256 _date, uint256 _startHour) external;
+}
