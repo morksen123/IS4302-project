524c05f777afc28cb3e3fdb492cf9e074397b3bb
Chris Chan <chrischanwz@gmail.com>
2024-11-19 23:39:16 +0800

    update

--- contracts/storage/ProposalStorage.sol
+++ contracts/storage/ProposalStorage.sol
@@ -46,27 +46,18 @@ contract ProposalStorage is DataStorageBase {
         return proposals.length - 1; // Return the ID of the newly stored proposal
     }
 
-    /// @notice Retrieve a proposal by its ID
-    /// @param proposalId The ID of the proposal to retrieve
-    /// @return The proposal data
+
     function getProposal(uint256 proposalId) external view onlyAuthorized returns (DataTypes.Proposal memory) {
         require(proposalId < proposals.length, "Invalid proposal ID");
         return proposals[proposalId];
     }
 
-    /// @notice Update the status of a proposal by its ID
-    /// @param proposalId The ID of the proposal to update
-    /// @param newStatus The new status to set for the proposal
+
     function updateProposalStatus(uint256 proposalId, DataTypes.ProposalStatus newStatus) external onlyAuthorized {
         require(proposalId < proposals.length, "Invalid proposal ID");
         proposals[proposalId].status = newStatus;
     }
 
-    /// @notice Increment the vote counts for a proposal
-    /// @param proposalId The ID of the proposal to update
-    /// @param votesFor The number of "for" votes to add
-    /// @param votesAgainst The number of "against" votes to add
-    /// @param votesAbstained The number of "abstain" votes to add
     function incrementVotes(
         uint256 proposalId,
         uint256 votesFor,
@@ -78,32 +69,21 @@ contract ProposalStorage is DataStorageBase {
         proposals[proposalId].votesAgainst += votesAgainst;
         proposals[proposalId].votesAbstained += votesAbstained;
     }
-
-    /// @notice Store a vote ID for a proposal
-    /// @param proposalId The ID of the proposal to associate the vote with
-    /// @param voteId The ID of the vote to store
+  
     function addVoteToProposal(uint256 proposalId, uint256 voteId) external onlyAuthorized {
         require(proposalId < proposals.length, "Invalid proposal ID");
         proposalVoteIds[proposalId].push(voteId);
     }
 
-    /// @notice Get all vote IDs associated with a proposal
-    /// @param proposalId The ID of the proposal to query
-    /// @return An array of vote IDs
     function getVoteIdsForProposal(uint256 proposalId) external view onlyAuthorized returns (uint256[] memory) {
         require(proposalId < proposals.length, "Invalid proposal ID");
         return proposalVoteIds[proposalId];
     }
 
-    /// @notice Retrieve all proposals
-    /// @return An array of all proposals
     function getAllProposals() external view onlyAuthorized returns (DataTypes.Proposal[] memory) {
         return proposals;
     }
 
-    /// @notice Retrieve proposals by their proposer (unit address)
-    /// @param unitAddress The address of the proposer
-    /// @return An array of proposals created by the given address
     function getProposalsByUnit(address unitAddress) external view onlyAuthorized returns (DataTypes.Proposal[] memory) {
         uint256 proposalCount = proposals.length;
         uint256 count = 0;
@@ -114,7 +94,6 @@ contract ProposalStorage is DataStorageBase {
                 count++;
             }
         }
-
         // Create an array to store the matching proposals
         DataTypes.Proposal[] memory result = new DataTypes.Proposal[](count);
         uint256 index = 0;
@@ -130,9 +109,6 @@ contract ProposalStorage is DataStorageBase {
         return result;
     }
 
-    /// @notice Retrieve proposals by status
-    /// @param status The status to filter proposals by
-    /// @return An array of proposals matching the given status
     function getProposalByStatus(DataTypes.ProposalStatus status) external view onlyAuthorized returns (DataTypes.Proposal[] memory) {
         uint256 proposalCount = proposals.length;
         uint256 count = 0;
@@ -158,9 +134,7 @@ contract ProposalStorage is DataStorageBase {
 
         return result;
     }
-
-    /// @notice Get the total count of proposals
-    /// @return The total number of proposals
+    
     function getProposalCount() external view onlyAuthorized returns (uint256) {
         return proposals.length;
     }
